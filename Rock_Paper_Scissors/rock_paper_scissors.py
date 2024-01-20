0fb980c3d9fa9f9925fa2b5a4204b2f86f685a33
LyudmilLilov <lusililov@gmail.com>
2024-01-21 00:49:36 +0200

    Created rock, paper or scissors game

--- Rock_Paper_Scissors/rock_paper_scissors.py
+++ Rock_Paper_Scissors/rock_paper_scissors.py
@@ -16,14 +16,14 @@ else:
     raise SystemExit("Invalid Input. Try again...")
 
 computer_random_number = random.randint(1, 3)
-computer_move = ""
+computer_move = f"{computer_random_number}"
 
-if player_move == "r":
-    player_move = rock
-elif player_move == "p":
-    player_move = paper
-elif player_move == "s":
-    player_move = scissors
+if computer_move == "1":
+    computer_move = rock
+elif computer_move == "2":
+    computer_move = paper
+elif computer_move == "3":
+    computer_move = scissors
 
 if(player_move == rock and computer_move == scissors) or \
         (player_move == paper and computer_move == rock) or \

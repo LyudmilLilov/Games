a7e69b85bf8180cf81e15b1bd403a85baa31a69d
LyudmilLilov <lusililov@gmail.com>
2024-01-23 22:43:30 +0200

    completed guess_a_number.py

--- Guess_A_Number/guess_a_number.py
+++ Guess_A_Number/guess_a_number.py
@@ -3,14 +3,18 @@ import random
 computer_number = random.randint(1, 100)
 
 while True:
-    player_input = input("Guess the number(1-100): ")
+    player_input = input("Guess the number (1-100): ")
 
-    if player_input.isdigit():
-        print("Invalid input. Try again...")
+    if not player_input.isdigit():
+        print("Invalid input. Please enter a valid number.")
         continue
 
-    player_number = int(input())
+    player_number = int(player_input)
 
     if player_number == computer_number:
-        print("You guess it!")
+        print("You guessed it!")
         break
+    elif player_number > computer_number:
+        print("Too High!")
+    else:
+        print("Too Low!")

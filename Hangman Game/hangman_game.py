fffcf630fc917ae79e860fc1453f71beacdaca20
LyudmilLilov <lusililov@gmail.com>
2024-02-13 23:14:32 +0200

    hangman_game.py

---
+++ Hangman Game/hangman_game.py
@@ -0,0 +1,46 @@
+import random
+
+# List of words to choose from
+words = ["python", "hangman", "computer", "programming", "challenge"]
+
+# Function to choose a random word
+def choose_word():
+    pass
+
+# Function to display the current state of the word with blanks
+def display_word(word, guessed_letters):
+    pass
+
+# Function to play Hangman
+def play_hangman():
+    pass
+
+    print("Welcome to Hangman!")
+
+    while attempts > 0:
+        print("\nWord: " + display_word(word_to_guess, guessed_letters))
+        print("Attempts left:", attempts)
+
+        guess = input("Guess a letter: ").lower()
+
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You've already guessed that letter.")
+            elif guess in word_to_guess:
+                print("Good guess!")
+                guessed_letters.append(guess)
+                if set(word_to_guess).issubset(set(guessed_letters)):
+                    print("Congratulations! You've guessed the word:", word_to_guess)
+                    break
+            else:
+                print("Wrong guess!")
+                guessed_letters.append(guess)
+                attempts -= 1
+        else:
+            print("Invalid input. Please enter a single letter.")
+
+    if attempts == 0:
+        print("You ran out of attempts. The word was:", word_to_guess)
+
+# Start the game
+play_hangman()
